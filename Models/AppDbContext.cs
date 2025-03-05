e7d6ff756896af38ef32120ea6b8b7640f2fb1a4
Minh Pham <giaminh2042004@gmail.com>
2025-03-05 16:08:18 +0700

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagementAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
